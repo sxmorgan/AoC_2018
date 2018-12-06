771562bea1ccba81e8e3bb4520bf6e59cb49d70a
Morgan Essex <messex@Morgans-MBP.fritz.box>
2018-12-06 11:11:25 +0100

    correct solution part I

--- day_2/day2.py
+++ day_2/day2.py
@@ -1,6 +1,23 @@
 import sys
+from itertools import groupby
 
 def main(argv):
 
+    # part I
+    fo = open(argv[1])
+    inlist = fo.read().splitlines()
+    fo.close()
+
+    print(checksum(inlist, 2) * checksum(inlist, 3))
+
+def checksum(inlist, num):
+    counts = 0
+    for s in range(len(inlist)):
+        sortstr = ''.join(sorted(inlist[s]))
+        temp = [list(g) for k,g in groupby(sortstr)]
+        lengths = [len(l) for l in temp]
+        if lengths.count(num) > 0:
+            counts += 1
+    return(counts)
 
 main(sys.argv)
