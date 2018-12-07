e9e213264b260d22c56ed6684e3d11a2ba122999
Morgan Essex <messex@Morgans-MBP.fritz.box>
2018-12-07 19:57:01 +0100

    keep request num

--- day_3/day3.R
+++ day_3/day3.R
@@ -1,14 +1,14 @@
 # part I
-data <- read.csv('~/GitHub/AoC_2018/day_3/clean_table.csv', header=FALSE)[,-c(1,6)]
-colnames(data) <- c('FromLeft','FromTop','Width','Height')
-cloth <- matrix(NA, nrow = 1000, ncol = 1000)
+data <- read.csv('~/GitHub/AoC_2018/day_3/clean_table_test.csv', header=FALSE)[,-6]
+colnames(data) <- c('Request','FromLeft','FromTop','Width','Height')
+cloth <- matrix(NA, nrow = 8, ncol = 8)
 
 for (i in 1:nrow(data)) {
     #print(data[i,])
-    cidx <- data[i,1]+1 
-    ridx <- data[i,2]+1
-    for (row in seq(ridx, ridx + data[i,4] - 1, 1)) {
-        for (col in seq(cidx, cidx + data[i,3] - 1, 1)){
+    cidx <- data[i,2]+1 
+    ridx <- data[i,3]+1
+    for (row in seq(ridx, ridx + data[i,5] - 1, 1)) {
+        for (col in seq(cidx, cidx + data[i,4] - 1, 1)){
             if (is.na(cloth[row, col])) {cloth[row, col] <- 0}
             else {cloth[row, col] <- 1}}}}
 
