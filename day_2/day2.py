740e29954a31d6c2ac150818464c7f67684626dd
Morgan Essex <messex@Morgans-MBP.fritz.box>
2018-12-07 14:37:52 +0100

    lazy part II solution

--- day_2/day2.py
+++ day_2/day2.py
@@ -3,12 +3,15 @@ from itertools import groupby
 
 def main(argv):
 
-    # part I
     fo = open(argv[1])
     inlist = fo.read().splitlines()
     fo.close()
 
+    # part I
     print(checksum(inlist, 2) * checksum(inlist, 3))
+
+    # part II
+    print(find_boxes(inlist))
 
 def checksum(inlist, num):
     counts = 0
@@ -19,5 +22,17 @@ def checksum(inlist, num):
         if lengths.count(num) > 0:
             counts += 1
     return(counts)
+
+def find_boxes(inlist):
+    for i in range(len(inlist)):
+        s1 = list(enumerate(inlist[i]))
+
+        for j in range(len(inlist[1:])):
+            s2 = list(enumerate(inlist[j]))
+            shared = set(s1) & set(s2)
+
+            if len(shared) == (len(s1)-1):
+                temp = [tup[1] for tup in shared]
+                return(inlist[i], inlist[j])
 
 main(sys.argv)
