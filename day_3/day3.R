17d60cca5378f330f40896c6525ee04f391a6417
Morgan Essex <messex@Morgans-MBP.fritz.box>
2018-12-08 11:16:22 +0100

    part II not working

--- day_3/day3.R
+++ day_3/day3.R
@@ -1,16 +1,31 @@
-# part I
-data <- read.csv('~/GitHub/AoC_2018/day_3/clean_table_test.csv', header=FALSE)[,-6]
-colnames(data) <- c('Request','FromLeft','FromTop','Width','Height')
-cloth <- matrix(NA, nrow = 8, ncol = 8)
+
+data <- read.csv('~/GitHub/AoC_2018/day_3/clean_table.csv', header=FALSE)[,-6]
+colnames(data) <- c('ID','FromLeft','FromTop','Width','Height')
+cloth <- matrix(NA, nrow = 1000, ncol = 1000)
 
 for (i in 1:nrow(data)) {
-    #print(data[i,])
     cidx <- data[i,2]+1 
     ridx <- data[i,3]+1
+    counter <- FALSE
+    
     for (row in seq(ridx, ridx + data[i,5] - 1, 1)) {
+        
         for (col in seq(cidx, cidx + data[i,4] - 1, 1)){
-            if (is.na(cloth[row, col])) {cloth[row, col] <- 0}
-            else {cloth[row, col] <- 1}}}}
+            
+            if (is.na(cloth[row, col])) {
+                cloth[row, col] <- data[i,1]}
+            else {
+                cloth[row, col] <- 0
+                counter <- TRUE}}}
+    
+    data[i,'Overlap'] <- counter}
 
+# check ID 1 eventually (would have no overlap...)
+
+# part I
 cut_cloth <- table(cloth)
-print(cut_cloth[names(cut_cloth) == 1])
+print(cut_cloth[names(cut_cloth) == 0])
+
+# part II
+print(data[which(data$Overlap == FALSE),]) # TOO MANY, WHY!?
+
