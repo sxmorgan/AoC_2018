a58e9ae8c233443dda2f552760551ca2a8b6734f
Morgan Essex <messex@Morgans-MBP.fritz.box>
2018-12-09 13:11:58 +0100

    slow but correct part II solution

--- day_3/day3.R
+++ day_3/day3.R
@@ -6,26 +6,40 @@ cloth <- matrix(NA, nrow = 1000, ncol = 1000)
 for (i in 1:nrow(data)) {
     cidx <- data[i,2]+1 
     ridx <- data[i,3]+1
-    counter <- FALSE
-    
+
     for (row in seq(ridx, ridx + data[i,5] - 1, 1)) {
         
         for (col in seq(cidx, cidx + data[i,4] - 1, 1)){
             
-            if (is.na(cloth[row, col])) {
-                cloth[row, col] <- data[i,1]}
-            else {
-                cloth[row, col] <- 0
-                counter <- TRUE}}}
-    
-    data[i,'Overlap'] <- counter}
+            if (is.na(cloth[row, col])) {cloth[row, col] <- data[i,1]}
+            else {cloth[row, col] <- 0}}}}
 
-# check ID 1 eventually (would have no overlap...)
+# for (i in 1:nrow(data)) {
+#     
+#     counts <- table(cloth)[which(names(table(cloth))==data[i,1])]
+#     area <- data[i,4]*data[i,5]
+#     if (length(counts) == 0) {data[i,'Overlap'] <- TRUE}
+#     else if (counts != area) {data[i,'Overlap'] <- TRUE}
+#     else {data[i,'Overlap'] <- FALSE}
+# }
 
 # part I
 cut_cloth <- table(cloth)
 print(cut_cloth[names(cut_cloth) == 0])
 
+areas <- data[,4]*data[,5]
+names(areas) <- rownames(data)
+overlap <- vapply(rownames(data), FUN=function(i) {
+    counts <- cut_cloth[which(names(table(cloth))==data[i,1])]
+    area <- areas[i]
+    if (length(counts) == 0) {
+        return(TRUE)}
+    else if (counts != area) {
+        return(TRUE)}
+    else {return(FALSE)}
+}, FUN.VALUE = logical(1))
+data$Overlap <- overlap
+
 # part II
-print(data[which(data$Overlap == FALSE),]) # TOO MANY, WHY!?
+print(data[which(data$Overlap == FALSE),1])
 
