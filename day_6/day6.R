0b2c92743b1b2444fd0e85e1bd201a1176868b54
Morgan Essex <messex@Morgans-MBP.fritz.box>
2018-12-25 22:25:43 -0500

    parts I and II

--- day_6/day6.R
+++ day_6/day6.R
@@ -1,18 +1,61 @@
 
 indata <- cbind(c(1,1,8,3,5,8), c(1,6,3,4,5,9)) # col, row !
-# indata <- read.csv('~/GitHub/AoC_2018/day_6/in.csv')
+indata <- read.csv('~/GitHub/AoC_2018/day_6/in.csv', header=FALSE)
 
 markers <- c(LETTERS, paste0(LETTERS, LETTERS))[1:nrow(indata)]
-rownames(indata)
+rownames(indata) <- markers
 
 grid.view <- matrix(NA, nrow = max(indata[,2])+1, ncol = max(indata[,1]+1))
 for (i in 1:nrow(indata)) {grid.view[indata[i,2], indata[i,1]] <- markers[i]}
-coords <- vapply(rownames(grid))
-for (r in 1:nrow(grid.view)) {
-    for (c in 1:ncol(grid.view)) {}
+
+# part I
+grid.filled <- fill.grid(indata, grid.view)
+areas <- find.area(grid.filled)
+letter <- names(areas)[which(areas==max(areas))]
+print(max(areas) + areas[toupper(letter)])
+
+
+# part II
+grid.bool <- total.distance(indata, grid.view, 10000)
+print(table(grid.bool))
+
+fill.grid <- function(coords, grid.view) {
+   
+    for (row in 1:nrow(grid.view)) {
+        for (col in 1:ncol(grid.view)) {
+            if (is.na(grid.view[row, col])) {
+                d.mat <- as.matrix(dist(rbind(coords, c(col, row)), 
+                                        method = 'manhattan'))
+                dists <- d.mat[nrow(d.mat),-ncol(d.mat)]
+                nearest <- tolower(names(dists[which(dists==min(dists))]))
+                #print(paste(row, col, nearest))
+                if (length(nearest) > 1) grid.view[row, col] <- '.'
+                else grid.view[row, col] <- nearest
+            }
+        }
+    }
+    
+    return(grid.view)
 }
 
-grid.view <- cbind(vapply(ncol(grid.view), FUN=function(c, coords){
-    col.coords <- 
-    dts <- dist(grid.view[,c], coords)
-}, FUN.VALUE = character(1), coords=indata))
+find.area <- function(grid.filled) {
+ 
+    counts <- table(grid.filled)
+    edges <- unique(c(unique(grid.filled[1,]), unique(grid.filled[nrow(grid.filled),]),
+                      unique(grid.filled[,1]), unique(grid.filled[,ncol(grid.filled)])))
+    options <- subset(counts, !(names(counts) %in% edges))
+    return(options)
+}
+
+total.distance <- function(coords, grid.view, limit) {
+    
+    for (row in 1:nrow(grid.view)) {
+        for (col in 1:ncol(grid.view)) {
+            tot.dist <- sum(abs(row-coords[,2]), abs(col-coords[,1]))
+            if (tot.dist < limit) grid.view[row, col] <- TRUE
+            else grid.view[row, col] <- FALSE
+        }
+    }
+    
+    return(grid.view)
+}
--- day_8/day8.R
+++ day_8/day8.R
@@ -1,56 +1,43 @@
 
 tree <- as.numeric(strsplit('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2', ' ')[[1]])
-tree <- as.numeric(strsplit('2 3 1 2 0 1 100 1 1 0 1 10 1 1 2', ' ')[[1]])
+tree <- as.numeric(strsplit('2 3 2 1 0 1 5 0 1 6 7 0 1 8 9 10 11', ' ')[[1]])
 filename = '~/GitHub/AoC_2018/day_8/in.txt'
 fo <- file(filename, open='r')
 tree <- as.numeric(strsplit(readLines(fo),' ')[[1]])
 close(fo)
 
-# initialize structure with root info
-thing <- list(tree = tree[3:length(tree)],  rem.children = tree[1],
-              rem.meta = tree[2], cum.meta = 0)
+cum.sum <<- 0
 
 # part I
-print(hnnngh(thing))
+hnnngh(tree)
+print(cum.sum)
 
-hnnngh <- function(thing) {
+hnnngh <- function(tree) {
 
-    tree <- thing$tree
-    i.children <- thing$tree[1]
-    thing$rem.children <- thing$rem.children - 1 + i.children #update int
-    #print('initial update of rem.children:')
-    #print(tree)
+    print('NEW ITERATION')
+    print(tree)
+    num.chil <- tree[1]
+    num.meta <- tree[2]
 
-    if (thing$rem.children == 0) # BASE CASE
-        return(sum(thing$cum.meta, 
-                   as.numeric(na.omit(tree[3:length(tree)]))))
+    # BASE CASE - metadata reached
+    if (num.chil == 0) {
+        print('base case reached')
+        print(tree[3:(2+num.meta)])
+        meta.sum <- sum(tree[3:(2+num.meta)])
+        print(paste('meta.sum',meta.sum))
+        cum.sum <<- cum.sum + meta.sum
+        print(cum.sum)}
     
-    else { # WORK TOWARD BASE CASE
-        i.metadata <- thing$tree[2]
-        thing$rem.meta <- thing$rem.meta + i.metadata #update int
-        
-        # rest of tree is metadata - second base case...?
-        if (thing$rem.meta == (length(tree) - 2)) { 
-            return(sum(thing$cum.meta, 
-                       as.numeric(na.omit(tree[3:length(tree)]))))}
-
-        # next seq is metadata
-        else if (i.children == 0) { 
-            idx <- i.metadata + 2 #for next iteration
-            thing$rem.meta <- thing$rem.meta - i.metadata
-            thing$cum.meta <- thing$cum.meta + sum(tree[3:idx])
-            thing$tree <- as.numeric(na.omit(tree[idx+1:length(tree)]))
-            #print('0 children, metadata follows:')
-            #print(thing)
-        }
-        
-        # next seq is child
-        else {
-            thing$tree <- as.numeric(na.omit(tree[3:length(tree)]))
-            #print('n children, child follows:')
-            #print(thing)
-            }
-            
-        return(hnnngh(thing))
-    }
+    # ELSE - create child trees
+    print(paste('calling hnnngh', num.chil, 'times'))
+    for (i in seq_len(num.chil)) {
+        tree <- as.numeric(na.omit(tree[3:length(tree)]))
+        print(tree)
+        hnnngh(tree)}
+    
+    #meta.sum <- sum(tree[1:num.meta])
+    meta.sum <- sum(tree[1:num.meta])
+    print(paste('meta.sum', meta.sum))
+    cum.sum <<- cum.sum + meta.sum
+    print(paste('end', cum.sum))
 }
